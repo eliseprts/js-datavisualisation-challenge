b58c42832d9bb9d86b8e2e795c58eceba79040c5
eliseprts <elisepourtois.pro@gmail.com>
2022-03-23 17:03:46 +0100

    add scriptelise.js : create element canvas

---
+++ challenge-js-files/script/scriptelise.js
@@ -0,0 +1,9 @@
+// INSERT BALISE CANVAS
+const mwContentText = document.getElementById('mw-content-text')
+const table1 = document.getElementById('table1')
+const canvas = document.createElement('canvas')
+canvas.setAttribute('id', 'canvas1')
+
+mwContentText.insertBefore(canvas, table1)
+
+
