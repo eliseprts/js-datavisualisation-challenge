a9f90163c8bb951e8920faad23de392b33249453
eliseprts <elisepourtois.pro@gmail.com>
2022-03-25 11:04:55 +0100

    upload work table1 25/3 am

--- challenge-js-files/script/table1.js
+++ challenge-js-files/script/table1.js
@@ -22,7 +22,9 @@ for (let i = 1; i < tr.length; i++) {
 
     let country = {}
 
-    country['name'] = tr[i].getElementsByTagName('td')[0].innerText
+    country['label'] = tr[i].getElementsByTagName('td')[0].innerText
+
+    country['borderColor'] = '#' + Math.floor(Math.random() * 16777215).toString(16)
 
     let numbers = []
 
@@ -33,26 +35,30 @@ for (let i = 1; i < tr.length; i++) {
         // parseFloat parses a value as a string and returns the first number
     }
 
-    country['numbers'] = numbers
+    country['data'] = numbers
 
     dataTable1.push(country)
 
 }
-
-console.log(dataTable1)
+// console.log(dataTable1)
 
 // GRAPH
+
+var randomIndex1 = Math.floor(Math.random() * 35)
+var randomIndex2 = Math.floor(Math.random() * 35)
+var randomIndex3 = Math.floor(Math.random() * 35)
+// console.log(randomIndex1)
+// console.log(randomIndex2)
+// console.log(randomIndex3)
 
 var ctx = document.getElementById('graph1').getContext('2d')
 
 var data = {
     labels: [2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012],
     datasets: [
-        {
-            label: 'Crimes recorded by the police',
-            borderColor: 'rgb(75, 192, 192)',
-            data: dataTable1[1].numbers
-        }
+        dataTable1[randomIndex1],
+        dataTable1[randomIndex2],
+        dataTable1[randomIndex3]
     ]
 }
 
@@ -62,3 +68,16 @@ var config = {
 }
 
 var graph1 = new Chart(ctx, config)
+
+// ANIMATION ?
+
+var countryName
+var countryNumbers
+
+tr[1].addEventListener('mouseover', function () {
+    countryName = dataTable1[0].name
+    console.log(countryName)
+    countryNumbers = dataTable1[0].numbers
+    console.log(countryNumbers)
+
+})
